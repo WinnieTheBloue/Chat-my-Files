39e73a9a1bd47a7debbaac7dc4ffc8ac5e3a2356
Nicolas Meuwly <nicomeuwly@mbp-de-nico.einet.ad.eivd.ch>
2023-11-28 14:21:24 +0100

    Complete methods for chatController and add routes

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,9 +1,37 @@
+import Message from '../models/message.js';
+
 const chatController = {
     async getMessages(req, res) {
+        try {
+            const messages = await Message.find();
+            res.status(200).json(messages);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     },
     async postMessage(req, res) {
+        try {
+            const message = new Message({
+                content: req.body.content,
+                author: req.body.author
+            });
+            await message.save();
+            res.status(201).json(message);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     },
     async deleteMessage(req, res) {
+        try {
+            const message = await Message.findByIdAndDelete(req.params.id);
+            if(!message) {
+                res.status(404).json({ message: 'Message not found' });
+                return;
+            }
+            res.status(200).json(message);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
 };
 
--- routes/chat.js
+++ routes/chat.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import * as chatController from '../controllers/chatController.js';
+import chatController from '../controllers/chatController.js';
 
 const router = express.Router();
 
-// router.get('/messages', chatController.getMessages);
-// router.post('/messages', chatController.postMessage);
-// router.delete('/messages/:id', chatController.deleteMessage);
+router.get('/messages', chatController.getMessages);
+router.post('/messages', chatController.postMessage);
+router.delete('/messages/:id', chatController.deleteMessage);
 
 export default router;
