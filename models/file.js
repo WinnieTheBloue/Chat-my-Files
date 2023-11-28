3e40aeafb4b3acd2c1e585167c1bea811c003024
Pablo Crotti <pablo.crotti@heig-vd.ch>
2023-11-28 12:57:55 +0100

    Models

---
+++ models/file.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const fileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  path: {
+    type: String,
+    required: true
+  },
+  uploadedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+});
+
+export default mongoose.model('File', fileSchema);
---
+++ models/user.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['Administrateur', 'Editeur', 'Lecteur', 'Invité', 'Désactivé'],
+    default: 'Désactivé'
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export default mongoose.model('User', userSchema);
