5a275396641173513a1cdd8bebaeef0cdb79c09e
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-01-10 13:58:44 +0100

    index

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+// import { set } from "mongoose"
+
+let nb = 1
+
+setInterval(function () {
+    const text = document.querySelector('.animated-text-in')
+    text.classList.remove('animated-text-in')
+    text.classList.add('animated-text-out')
+
+    nb++
+
+    if (nb > 5) {
+        nb = 1
+    }
+
+    const id = `an-t-${nb}`
+
+    const text2 = document.getElementById(id)
+    text2.classList.remove('animated-text-out')
+    text2.classList.add('animated-text-in')
+
+}, 2000)
