941a81bdc8591ae01ffe2a2fa742ebd916b2764f
tarahachler <92160920+tarahachler@users.noreply.github.com>
2024-01-14 11:30:04 +0100

    adding random file name

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,9 +1,9 @@
-import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
-import { promises as fsPromises } from 'fs';
-import util from 'util';
-import { fileURLToPath } from 'url';
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+import { promises as fsPromises } from "fs";
+import util from "util";
+import { fileURLToPath } from "url";
 
 // Utilize util.promisify to convert fs.readdir into a promise-based function
 const readdirAsync = util.promisify(fs.readdir);
@@ -11,21 +11,27 @@ const readdirAsync = util.promisify(fs.readdir);
 // Define the maximum file size for uploads
 const maxSize = 2 * 1024 * 1024;
 // Specify the allowed file types for uploads
-const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'application/pdf'];
+const allowedTypes = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "application/pdf",
+];
 
 // Determine the filename and directory of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // Set the directory path for storing uploaded files
-const uploadDir = path.join(__dirname, '../uploaded_files');
+const uploadDir = path.join(__dirname, "../uploaded_files");
 
 /**
  * Asynchronously ensures the existence of the upload directory, creating it if it does not exist.
  */
 async function ensureUploadDir() {
   try {
-    await fsPromises.mkdir(uploadDir, { recursive: true });  } catch (error) {
-    console.error('Error creating upload directory:', error);
+    await fs.mkdir(uploadDir, { recursive: true });
+  } catch (error) {
+    console.error("Error creating upload directory:", error);
   }
 }
 
@@ -37,8 +43,13 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
+    // Generate a random filename to avoid potential security issues
+    const randomFileName = `${Date.now()}_${Math.floor(
+      Math.random() * 10000
+    )}_${file.originalname}`;
+
+    cb(null, randomFileName);
+  },
 });
 
 /**
@@ -48,7 +59,7 @@ const fileFilter = (req, file, cb) => {
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error('Invalid file type'), false);
+    cb(new Error("Invalid file type"), false);
   }
 };
 
@@ -56,8 +67,8 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: maxSize }
-}).single('file');
+  limits: { fileSize: maxSize },
+}).single("file");
 
 const fileController = {
   /**
@@ -73,14 +84,21 @@ const fileController = {
   async listFiles(req, res) {
     try {
       const currentModulePath = fileURLToPath(import.meta.url);
-      const folderPath = path.join(path.dirname(currentModulePath), '../uploaded_files');
+      const folderPath = path.join(
+        path.dirname(currentModulePath),
+        "../uploaded_files"
+      );
 
       const files = await readdirAsync(folderPath);
 
       return files;
     } catch (err) {
       console.error(err);
-      res.status(500).send('Une erreur est survenue lors de la récupération de la liste des fichiers.');
+      res
+        .status(500)
+        .send(
+          "Une erreur est survenue lors de la récupération de la liste des fichiers."
+        );
     }
   },
 
@@ -101,15 +119,15 @@ const fileController = {
           return res.status(400).send(err.message);
         } else {
           console.error(err);
-          return res.status(500).send('Internal Server Error');
+          return res.status(500).send("Internal Server Error");
         }
       }
 
       if (!req.file) {
-        return res.status(400).send('No file provided');
+        return res.status(400).send("No file provided");
       }
 
-      return res.redirect('/files');
+      return res.redirect("/files");
     });
   },
 
@@ -124,60 +142,76 @@ const fileController = {
    */
   async downloadFile(req, res) {
     try {
-      const filename = req.params.filename;
-      const filePath = path.join(__dirname, '../uploaded_files', filename);
+      const filename = decodeURIComponent(req.params.filename);
+
+      // Validate the filename to prevent directory traversal
+      if (!/^[a-zA-Z0-9_.-]+$/.test(filename)) {
+        return res.status(400).send("Invalid filename");
+      }
+
+      const filePath = path.join(__dirname, "../uploaded_files", filename);
 
       console.log(`Attempting to download file at path: ${filePath}`);
 
       try {
         await fsPromises.access(filePath);
-        res.download(filePath, filename);
+
+        // Set appropriate headers to force download
+        res.setHeader(
+          "Content-disposition",
+          `attachment; filename=${encodeURIComponent(filename)}`
+        );
+        res.setHeader("Content-type", "application/octet-stream");
+
+        // Stream the file to the response
+        const fileStream = fs.createReadStream(filePath);
+        fileStream.pipe(res);
       } catch (error) {
         console.error(`Error accessing file: ${error}`);
-        if (error.code === 'ENOENT') {
-          res.status(404).send('File not found.');
+        if (error.code === "ENOENT") {
+          res.status(404).send("File not found.");
         } else {
           throw error;
         }
       }
     } catch (error) {
       console.error(`Error in downloadFile: ${error}`);
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     }
   },
 
   /**
-  * Delete a file from the server.
-  *
-  * @async
-  * @function
-  * @memberof fileController
-  * @param {Object} req - Express request object.
-  * @param {Object} res - Express response object.
-  */
+   * Delete a file from the server.
+   *
+   * @async
+   * @function
+   * @memberof fileController
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   */
   async deleteFile(req, res) {
     try {
       const filename = req.params.filename;
       const __filename = fileURLToPath(import.meta.url);
       const __dirname = path.dirname(__filename);
-      const filePath = path.join(__dirname, '../uploaded_files', filename);
+      const filePath = path.join(__dirname, "../uploaded_files", filename);
 
       try {
         await fsPromises.access(filePath);
         await fsPromises.unlink(filePath);
-        return res.redirect('/files');
+        return res.redirect("/files");
       } catch (fileError) {
-        if (fileError.code === 'ENOENT') {
-          res.status(404).send('File not found.');
+        if (fileError.code === "ENOENT") {
+          res.status(404).send("File not found.");
         } else {
           throw fileError;
         }
       }
     } catch (error) {
       console.error(error);
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     }
-  }
+  },
 };
 
 export default fileController;
--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,36 +1,9 @@
-const handleErrors = (err, req, res, next) => {
-    console.error(err); 
 
-    if (req.xhr || req.headers.accept.indexOf('json') > -1) {
-
-        let statusCode = 500; 
-        let errorMessage = 'Internal Server Error';
-
-        if (err.status === 400) {
-            statusCode = 400;
-            errorMessage = err.message || 'Bad Request';
-        } else if (err.status === 403) {
-            statusCode = 403;
-            errorMessage = err.message || 'Forbidden';
-        }
-
-        return res.status(statusCode).json({ error: errorMessage });
-    }
-
-    res.locals.error = err;
-
-    let statusCode = 500; 
-    let errorMessage = 'Internal Server Error';
-
-    if (err.status === 400) {
-        statusCode = 400;
-        errorMessage = err.message || 'Bad Request';
-    } else if (err.status === 403) {
-        statusCode = 403;
-        errorMessage = err.message || 'Forbidden';
-    }
-
-    res.status(statusCode).render('error', { error: errorMessage });
-};
-
+function handleErrors(err, req, res, next) {
+    console.error(err.stack);
+  
+    // Envoyer l'erreur à la vue EJS
+    res.status(500).render('error', { error: err.message });
+  }
+  
 export default handleErrors;
